c9d8feb31d7612fc50990608eee24d5aba44fb2d
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-12-01 14:39:55 +0100

    better error handling

--- previousWork/MOIM/main.py
+++ previousWork/MOIM/main.py
@@ -31,19 +31,14 @@ def run_bench():
 
 
 def change_limit_cgroup_file(cgroup_limit):
-    """
-    Change the limit of the cgroup in the file /sys/fs/cgroup/machine.slice/machine-qemu\x2d1\x2ddebian12\x2d1.scope/libvirt/memory.max
-    You may need to change the name of the VM in the path
-
-    :param cgroup_limit: new limit of the cgroup
-    :return: None - update the file
-    """
+    cgroup_file_path = r"/sys/fs/cgroup/machine.slice/machine-qemu-1-debian12-1.scope/libvirt/memory.max"
     try:
-        with open("/sys/fs/cgroup/machine.slice/machine-qemu\x2d1\x2ddebian12\x2d1.scope/libvirt/memory.max",
-                  "w") as fmax:
+        with open(cgroup_file_path, "w") as fmax:
             fmax.write(cgroup_limit)
-    except PermissionError:
-        print("Erreur de permission. Exécutez le script avec des privilèges élevés.")
+    except FileNotFoundError as e:
+        print(f"File not found - {e}")
+    except PermissionError as e:
+        print(f"Run the script with administrator privileges - {e}")
 
 
 def write_output(time, mem_limit, response_time, action):
