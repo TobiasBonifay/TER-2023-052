2fbd6bfcd5731c7da06e768e01e41d1e058c4782
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-12-01 13:33:34 +0100

    main

--- previousWork/MOIM/main.py
+++ previousWork/MOIM/main.py
@@ -1,6 +1,9 @@
 import time
 import numpy as np
 from threading import Thread
+
+from keras.src.layers import RNN
+
 from log import Log
 from memory_getter import MemoryGetter
 from client_mem_vm import ClientMemVM
@@ -8,20 +11,21 @@ from apache_benchmark import Benchmark
 from inference import Inference
 from colorama import init, Fore, Back, Style
 
+
 DURATION = 99999
 FINESSE = 0.5
-TR = 0
-SUEIL_TR = 500
+TIME_RESPONSE = 0
+THRESHOLD = 500
 
 b = Benchmark()
 log_ = Log()
 
 
 def run_bench():
-    global time_reponse
-    time_reponse = 0
+    global time_response
+    time_response = 0
     while True:
-        time_reponse = b.start_benchmark()
+        time_response = b.start_benchmark()
 
 
 def change_limit_cgroup_file(cgroup_limit):
@@ -29,9 +33,9 @@ def change_limit_cgroup_file(cgroup_limit):
         fmax.write(cgroup_limit)
 
 
-def write_output(time, mem_limit, tr, action):
+def write_output(time, mem_limit, response_time, action):
     with open("out", "a") as f_out:
-        f_out.write("{},{},{},{}\n".format(time, mem_limit, tr, action))
+        f_out.write("{},{},{},{}\n".format(time, mem_limit, response_time, action))
 
 
 class Mechanism:
@@ -40,8 +44,8 @@ class Mechanism:
     curr_tr_value = 0
     tr_list = np.zeros((0))
     t = 0
-    coeff_first_seuil = 1.3
-    coeff_second_seuil = 1.7
+    first_threshold_coefficient = 1.3
+    second_threshold_coefficient = 1.7
 
     def __init__(self, memorygetter, clientMemVm):
         self.memorygetter = memorygetter
@@ -51,14 +55,14 @@ class Mechanism:
     def run(self):
 
         start_time = time.process_time()
-        write_output("time", "mem_limit", "time_reponse", "action")
+        write_output("time", "mem_limit", "time_response", "action")
 
         self.t = round(time.process_time() - start_time, 2)
         Thread(target=run_bench).start()
         while self.t < DURATION:
             diff_t = round(time.process_time() - self.t - start_time, 2)
-            #print("DEBUG " + str(diff_t) + " >= " + str(FINESSE) + " and " + str(time_reponse) + " > 0")
-            if diff_t >= FINESSE and time_reponse > 0:  # we wait for the first value of the benchmark to perform an inference
+            print("DEBUG " + str(diff_t) + " >= " + str(FINESSE) + " and " + str(time_response) + " > 0")
+            if diff_t >= FINESSE and time_response > 0:  # we wait for the first value of the benchmark to perform an inference
                 print("OK")
                 self.t += diff_t
                 self.update_bash()
@@ -66,68 +70,83 @@ class Mechanism:
                 print(Style.BRIGHT + Fore.GREEN + "[t = {} s]".format(int(self.t)), end="\r")
 
     def add_tr_to_list_tr(self, tr):
+        """
+        Add a new tr value to the list of tr values
+        :param tr: time response value
+        :return: None - update the list of tr values
+        """
         if self.tr_list.shape[0] < 10 and tr != 0:
             self.tr_list = np.append(self.tr_list, tr)
         elif tr != 0:
             self.tr_list = np.roll(self.tr_list, -1)
             self.tr_list[-1] = tr
 
-    def SEUIL(self):
-        seuil1, seuil2 = np.mean(self.tr_list) * self.coeff_first_seuil, np.mean(self.tr_list) * self.coeff_second_seuil
-        log_.debug("SEUIL1 = {} - SEUIL2={}".format(seuil1, seuil2))
+    def get_threshold(self):
+        """
+        Compute the threshold for the prediction of the next tr value
+        :return: threshold_1, threshold_2
+        """
+        threshold_1, threshold_2 = np.mean(self.tr_list) * self.first_threshold_coefficient, np.mean(self.tr_list) * self.second_threshold_coefficient
+        log_.debug("Threshold 1 = {} - Threshold 2 = {}".format(threshold_1, threshold_2))
         log_.debug(self.tr_list)
-        return seuil1, seuil2
+        return threshold_1, threshold_2
 
     def update_bash(self):
         if np.array(self.bash_tmp).shape[0] < 25:
             self.bash_tmp.append([
-                self.memorygetter.get_mem_proc() / 1048576,
-                self.clientMemVm.get_used_memory() / 1024,
-                self.memorygetter.get_limit_cgroup() / 1048576,
-                time_reponse
+                self.memorygetter.get_mem_proc() / 1048576,  # in MB
+                self.clientMemVm.get_used_memory() / 1024,  # in kB
+                self.memorygetter.get_limit_cgroup() / 1048576,  # in MB
+                time_response  # in ms
             ])
         else:
             self.bash = self.bash_tmp
             self.bash_tmp = []
 
     def do_inference(self):
-        # we do prediction only when we have a new time response value that prevent us to cut lower the cgroup limit
-        # too frequently and make a prediction with tr value that are not descriptive of the VM state
-        if self.curr_tr_value != time_reponse and np.array(self.bash).shape[0] == 25:
+        """
+        We do prediction only when we have a new time response value that prevent us to cut lower the cgroup limit
+        too frequently and make a prediction with tr value that are not descriptive of the VM state
+        :return: None - update the cgroup limit
+        """
+        if self.curr_tr_value != time_response and np.array(self.bash).shape[0] == 25:
             self.add_tr_to_list_tr(self.curr_tr_value)
             self.do_predict()
-            self.curr_tr_value = time_reponse
+            self.curr_tr_value = time_response
 
     def do_predict(self):
+        """
+        Do the prediction of the next tr value
+        :return: None - update the cgroup limit
+        """
         x_bash = np.array([self.bash])
-        x_bash[0, :, -1] = time_reponse
+        x_bash[0, :, -1] = time_response
         tr_predict = self.infer.predict(x_bash)
         log_.output("(tr predict is - " + str(tr_predict[0]) + ") (tr current is - " + str(self.curr_tr_value) + ")")
         self.change_limit_cgroup(tr_predict[0])
 
     def change_limit_cgroup(self, tr_predict):
 
-        seuil1, seuil2 = self.SEUIL()
+        threshold_1, threshold_2 = self.get_threshold()
 
-        if tr_predict < seuil1:
+        if tr_predict < threshold_1:
             new_limit = int(self.memorygetter.get_limit_cgroup() * 0.9)
             change_limit_cgroup_file(str(new_limit))
             log_.debug("limit_cgroup - " + str(int(self.memorygetter.get_limit_cgroup() / 1024)) + " kB")
             print(Style.BRIGHT + Fore.GREEN + "FREED MEMORY")
-            write_output(round(self.t, 2), int(self.memorygetter.get_limit_cgroup()) / 1048576, time_reponse, -1)
+            write_output(round(self.t, 2), int(self.memorygetter.get_limit_cgroup()) / 1048576, time_response, -1)
 
-        elif seuil1 < tr_predict < seuil2 or self.tr_list.shape[0] < 1:
+        elif threshold_1 < tr_predict < threshold_2 or self.tr_list.shape[0] < 1:
             print(Fore.YELLOW + "DO NOTHING")
-            write_output(round(self.t, 2), int(self.memorygetter.get_limit_cgroup() / 1048576), time_reponse, 0)
+            write_output(round(self.t, 2), int(self.memorygetter.get_limit_cgroup() / 1048576), time_response, 0)
 
         else:
-
             new_limit = int(self.memorygetter.get_limit_cgroup() * 2)
             change_limit_cgroup_file(str(new_limit))
             log_.debug("limit_cgroup - " + str(int(self.memorygetter.get_limit_cgroup() / 1024)) + " kB")
 
             print(Style.BRIGHT + Fore.RED + "ADD MEMORY")
-            write_output(round(self.t, 2), int(self.memorygetter.get_limit_cgroup() / 1048576), time_reponse, 1)
+            write_output(round(self.t, 2), int(self.memorygetter.get_limit_cgroup() / 1048576), time_response, 1)
 
 
 if __name__ == "__main__":
@@ -152,8 +171,9 @@ if __name__ == "__main__":
     mechanism.run()
     print("8")
 
-# cut_value = memory.current * 0.75
-# if RNN(cut_value) > SUEIL:
-#    memory.max *= 1.25
-# else:
-#    memory.max = cut_value
+cut_value = memory.current * 0.75
+if RNN(cut_value) > THRESHOLD:
+   memory.max *= 1.25
+else:
+   memory.max = cut_value
+
