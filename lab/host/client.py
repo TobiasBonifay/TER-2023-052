7ef968e85f16fca5ff5f45e7a5043b920dbd103e
tobias <tobias.bonifay@etu.unice.fr>
2024-02-06 14:47:03 +0100

    client extract, csv runtime and training split

---
+++ lab/host/client.py
@@ -0,0 +1,22 @@
+import socket
+
+
+class Client:
+    """
+    This class is used to get the information from the VMs.
+    Do the connection to the VMs and get the data.
+    """
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.host, self.port))
+
+    def get_data(self):
+        self.client.send(b"GET")
+        data = self.client.recv(4096)
+        return data.decode()
+
+    def close(self):
+        self.client.close()
--- lab/host/main.py
+++ lab/host/main.py
@@ -1,45 +1,40 @@
 import csv
 import re
-import socket
 import time
 
 import numpy as np
 import tensorflow as tf
 
-# Constants and configurations
-VM1_PATH_CGROUP_FILE = "/sys/fs/cgroup/machine.slice/machine-qemu\\x2d6\\x2dubuntu22.04.scope/memory.max"
+from lab.host.client import Client
+
+# VM 1: Apache server VM
+VM1_PATH_CGROUP_FILE = "/sys/fs/cgroup/machine.slice/machine-qemu\\x2d6\\x2dubuntu22.04.scope"
+VM1_CGROUP_MEMORY_MAX = VM1_PATH_CGROUP_FILE + "/memory.max"
+VM1_CGROUP_MEMORY_CURRENT = VM1_PATH_CGROUP_FILE + "/memory.current"
 VM1_HOST = '192.168.100.175'  # IP of VM1
-VM2_HOST = '192.168.100.231'  # IP of VM2
 VM1_PORT = 8000
+
+# VM 2: Client VM
+VM2_HOST = '192.168.100.231'  # IP of VM2
 VM2_PORT = 8000
+
+# Constants
 DURATION = 99999
 FINESSE = 0.5
 MODEL_PATH = '.'
 CSV_FILE = 'vm_data.csv'
+RUNTIME_ACTIONS_FILE = 'runtime_actions.csv'
 DEFAULT_THRESHOLD_1 = 2000
 DEFAULT_THRESHOLD_2 = 2000
 MIN_CGROUP_LIMIT = 1000000  # 1GB
 
 
-# Client class to interact with VMs
-class Client:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((self.host, self.port))
-
-    def get_data(self):
-        self.client.send(b"GET")
-        data = self.client.recv(4096)
-        return data.decode()
-
-    def close(self):
-        self.client.close()
-
-
-# Function to parse memory info
 def parse_memory_info(meminfo):
+    """
+    Return the memory used by the VM in bytes.
+    The input is the output of /proc/meminfo file in the VM (as a string).
+    This value is calculated as the difference between the total memory and the sum of free, buffered and cached memory.
+    """
     lines = meminfo.split('\n')
     mem_total = mem_free = mem_buffers = mem_cached = 0
     for line in lines:
@@ -56,7 +51,6 @@ def parse_memory_info(meminfo):
     return mem_used
 
 
-# Function to load TensorFlow model
 def load_model():
     try:
         model = tf.keras.models.load_model(MODEL_PATH)
@@ -73,9 +67,8 @@ def get_vm2_data(client):
 
 
 def change_cgroup_limit(new_limit):
-    cgroup_file_path = VM1_PATH_CGROUP_FILE
     try:
-        with open(cgroup_file_path, "w") as f:
+        with open(VM1_CGROUP_MEMORY_MAX, "w") as f:
             f.write(str(new_limit))
     except FileNotFoundError as e:
         print(f"File not found - {e}")
@@ -85,36 +78,33 @@ def change_cgroup_limit(new_limit):
         exit(1)
 
 
-def get_cgroup_memory_limit(cgroup_file_path):
+def get_cgroup_memory_limit():
     try:
-        with open(cgroup_file_path, 'r') as file:
-            limit = file.read().strip()
-            if limit == 'max':
-                return float('inf')
-            return int(limit)
+        with open(VM1_CGROUP_MEMORY_CURRENT, 'r') as file:
+            current_usage = file.read().strip()
+            return int(current_usage)
     except IOError as e:
-        print(f"Error reading {cgroup_file_path}: {e}")
+        print(f"Error reading {VM1_CGROUP_MEMORY_CURRENT}: {e}")
         return None
 
 
 def adjust_cgroup_limit(predicted_value, mem_vm_view):
-    action_taken = None
     if predicted_value < DEFAULT_THRESHOLD_1:
-        new_limit = max(int(mem_vm_view * (1 + FINESSE)),
-                        MIN_CGROUP_LIMIT)
+        new_limit = max(int(mem_vm_view * (1 + FINESSE)), MIN_CGROUP_LIMIT)
         change_cgroup_limit(new_limit)
-        action_taken = "Increase"
+        return -1
     elif predicted_value >= DEFAULT_THRESHOLD_2:
         new_limit = max(int(mem_vm_view * (1 - FINESSE)), MIN_CGROUP_LIMIT)
         change_cgroup_limit(new_limit)
-        action_taken = "Decrease"
-    return action_taken
+        return 1
+    else:
+        return 0
 
 
-# Main function
-def get_memory_host_view(client_vm1):
-    meminfo = client_vm1.get_data()  # TODO: This needs to be actual host view memory data
-    return parse_memory_info(meminfo)
+def log_runtime_action(time, mem_limit, action_taken):
+    with open(RUNTIME_ACTIONS_FILE, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([time, mem_limit, action_taken])
 
 
 def main():
@@ -122,41 +112,52 @@ def main():
     client_vm2 = Client(VM2_HOST, VM2_PORT)  # Client VM
     model = load_model()
 
+    # Initialize the runtime actions log file
+    with open(RUNTIME_ACTIONS_FILE, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Time', 'Memory Limit', 'Action Taken'])
+
+    # Initialize the training data log file
     with open(CSV_FILE, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(
-            ['Time', 'Memory (VM view)', 'Memory (Host view)', 'CT', 'BW (Download)', 'BW (Upload)', 'Cgroup Action'])
+        writer.writerow(['Time', 'Memory (VM view)', 'Memory (Host view)', 'CT', 'BW (Download)', 'BW (Upload)'])
 
-        t = 0
-        start_time = time.time()
-        while t < DURATION:
-            try:
-                current_time = time.time()
-                elapsed_time = current_time - start_time
+    t = 0
+    start_time = time.time()
+    while t < DURATION:
+        try:
+            current_time = time.time()
+            elapsed_time = current_time - start_time
 
-                if elapsed_time >= FINESSE:
-                    t += elapsed_time
-                    start_time = current_time
+            if elapsed_time >= FINESSE:
+                t += elapsed_time
+                start_time = current_time
 
-                    # Fetch data from VMs
-                    mem_vm_view = parse_memory_info(client_vm1.get_data())
-                    mem_host_view = get_memory_host_view(client_vm1)
-                    response_time, bw_download, bw_upload = get_vm2_data(client_vm2)
+                # Fetch data from VMs
+                mem_vm_view = parse_memory_info(client_vm1.get_data())
+                mem_host_view = get_cgroup_memory_limit()
+                response_time, bw_download, bw_upload = get_vm2_data(client_vm2)
 
-                    # Model inference and cgroup adjustments
-                    data_for_inference = np.array([[mem_vm_view, response_time, bw_download, bw_upload]])
-                    predicted_value = model.predict(data_for_inference)
-                    action_taken = adjust_cgroup_limit(predicted_value, mem_vm_view)
+                # Model inference and cgroup adjustments
+                data_for_inference = np.array([[mem_vm_view, response_time, bw_download, bw_upload]])
+                predicted_value = model.predict(data_for_inference)
+                action_taken = adjust_cgroup_limit(predicted_value, mem_vm_view)
 
-                    # Write data to CSV
-                    writer.writerow(
-                        [t, mem_vm_view, mem_host_view, response_time, bw_download, bw_upload, action_taken])
+                # Write data to the training log
+                writer.writerow([t, mem_vm_view, mem_host_view, response_time, bw_download, bw_upload])
 
-            except Exception as e:
-                print(f"An error occurred: {e}")
+                # Log the runtime action
+                log_runtime_action(t, get_cgroup_memory_limit(), action_taken)
 
-        client_vm1.close()
-        client_vm2.close()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    client_vm1.close()
+    client_vm2.close()
+
+
+def log_training(bw_download, bw_upload, mem_host_view, mem_vm_view, response_time, t, writer):
+    writer.writerow([t, mem_vm_view, mem_host_view, response_time, bw_download, bw_upload])
 
 
 if __name__ == "__main__":
