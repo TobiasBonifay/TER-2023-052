bbaebc252a2e785e93d17d4fb56f74fce41096d9
tobias <tobias.bonifay@etu.unice.fr>
2024-02-12 14:32:57 +0100

    setups

---
+++ lab/apache2/setup.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install apache2 -y
+sudo systemctl enable apache2
+
+sudo apt install git -y
+git clone https://github.com/TobiasBonifay/TER-2023-052.git
+cd TER-2023-052/lab/ || exit 1
+sudo apt install -y python3-pip
+pip3 install -r requirements.txt
+sudo python3 -m lab.apache2.TcpMemProc
---
+++ lab/client/setup.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install apache2-utils -y
+
+sudo apt install git -y
+git clone https://github.com/TobiasBonifay/TER-2023-052.git
+cd TER-2023-052/lab/ || exit 1
+sudo apt install -y python3-pip
+pip3 install -r requirements.txt
+sudo python3 -m lab.client.TcpBandwidth
