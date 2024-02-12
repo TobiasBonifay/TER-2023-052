59530be5c2fd3b62c1deb7aea0ab9f80f43267b7
tobias <tobias.bonifay@etu.unice.fr>
2024-02-12 14:57:48 +0100

    IP

--- lab/client/setup.sh
+++ lab/client/setup.sh
@@ -9,5 +9,15 @@ sudo apt install git -y
 git clone https://github.com/TobiasBonifay/TER-2023-052.git
 cd TER-2023-052/lab/ || exit 1
 sudo apt install -y python3-pip
-pip3 install -r requirements.txt
+
+# pip install -r requirements.txt if error continue the script
+output=$(pip3 install -r requirements.txt 2>&1)
+if [ $? -ne 0 ]; then
+    echo "Error: $output"
+fi
+
+# ip a and wait user to choose the interface and edit the config.json
+ip a
+nano config.json
+
 sudo python3 -m lab.client.TcpBandwidth
