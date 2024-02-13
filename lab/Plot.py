ccc62d8b14ad37c3b08608be20ee2df9a4659172
tobias <tobias.bonifay@etu.unice.fr>
2024-02-13 14:00:29 +0100

    oops

--- lab/Plot.py
+++ lab/Plot.py
@@ -24,23 +24,24 @@ data['Time'] = pd.to_datetime(data['Time'], unit='s')
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 15))
 
 # First plot for bandwidth
-ax1.plot(data['Time'], data['BW (Download)'], label='Download Bandwidth', color='blue')
-ax1.plot(data['Time'], data['BW (Upload)'], label='Upload Bandwidth', color='red')
+MEGA = 1024 * 1024
+ax1.plot(data['Time'], data['BW (Download)'] / MEGA, label='Download Bandwidth', color='blue')
+ax1.plot(data['Time'], data['BW (Upload)'] / MEGA, label='Upload Bandwidth', color='red')
 ax1.set_xlabel('Time')
-ax1.set_ylabel('Bandwidth (bytes)')
+ax1.set_ylabel('Bandwidth (Mo/s)')
 ax1.set_title('Bandwidth Usage Over Time')
 ax1.legend()
 ax1.grid(True)
 
 # Second plot for memory usage
-ax2.plot(data['Time'], data['Memory (VM view)'], label='Memory Usage (VM view)', color='green')
-ax2.plot(data['Time'], data['Memory (Host view)'] / 1000, label='Memory Limit (Host view)', color='orange')
+ax2.plot(data['Time'], data['Memory (VM view)'] / MEGA, label='Memory Usage (VM view)', color='green')
+ax2.plot(data['Time'], data['Memory (Host view)'] / MEGA, label='Memory Limit (Host view)', color='orange')
+ax2.plot(data['Time'], data['Swap used (Host view)'] / (MEGA * 10), label='Swap Usage / 10', color='red')
 ax2.set_xlabel('Time')
-ax2.set_ylabel('Memory (bytes)')
+ax2.set_ylabel('Memory (Mo)')
 ax2.set_title('Memory Usage Over Time')
 ax2.legend()
 ax2.grid(True)
 
 # Show the plots
-plt.tight_layout()
 plt.show()
