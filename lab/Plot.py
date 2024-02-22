8328d2f50a125efde23dfa8a5ae7d315ded7f672
tobias <tobias.bonifay@etu.unice.fr>
2024-02-22 10:44:17 +0100

    sample 5

--- lab/Plot.py
+++ lab/Plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from lab.common.Constants import BANDWIDTH_UPLOAD_VM_, BANDWIDTH_DOWNLOAD_VM_, RESPONSE_TIME_VM_, \
-    SWAP_HOST_, MEMORY_HOST_, C_GROUP_LIMIT_VM_, MEMORY_USED_VM_
+    SWAP_HOST_, MEMORY_HOST_, C_GROUP_LIMIT_VM_
 
 
 # Function to get the latest file in a directory
@@ -25,26 +25,27 @@ data = pd.read_csv(CSV_FILE)
 data['Time'] = pd.to_datetime(data['Time'], unit='s')
 
 # Setting up the subplots
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(15, 12))
+ax1_2 = ax1.twinx()
+fig.patch.set_alpha(0.0)
 
 # First plot for bandwidth
 MEGA = 1024 * 1024
+ax1_2.plot(data['Time'], data[BANDWIDTH_UPLOAD_VM_], '-o', label=BANDWIDTH_UPLOAD_VM_, color='red')
 ax1.plot(data['Time'], data[BANDWIDTH_DOWNLOAD_VM_], '-o', label=BANDWIDTH_DOWNLOAD_VM_, color='blue')
-ax1.plot(data['Time'], data[BANDWIDTH_UPLOAD_VM_] * 20, '-o', label=BANDWIDTH_UPLOAD_VM_ + " x20", color='green')
-ax1.set_xlabel('Time')
 ax1.set_ylabel('Bandwidth (Mo/s)')
 ax1.set_title('Bandwidth Usage Over Time')
-ax1.legend(loc='upper left', bbox_to_anchor=(0, 1.2), ncol=2)
+ax1.legend(loc='upper left')
+ax1_2.legend(loc='upper right')
 ax1.grid(True)
 
 # Second plot for memory usage
 ax2.plot(data['Time'], data[C_GROUP_LIMIT_VM_] / MEGA, '-o', label=C_GROUP_LIMIT_VM_, color='red')
 # ax2.plot(data['Time'], data[MEMORY_TOTAL_VM_] / MEGA, label=MEMORY_TOTAL_VM_, color='blue')
 # ax2.plot(data['Time'], data[MEMORY_AVAILABLE_VM_] / MEGA, label=MEMORY_AVAILABLE_VM_, color='green')
-ax2.plot(data['Time'], data[MEMORY_USED_VM_] / MEGA, '-o', label=MEMORY_USED_VM_, color='green')
+# ax2.plot(data['Time'], data[MEMORY_USED_VM_] / MEGA, '-o', label=MEMORY_USED_VM_, color='green')
 ax2.plot(data['Time'], data[MEMORY_HOST_] / MEGA, '-o', label=MEMORY_HOST_, color='purple')
 ax2.plot(data['Time'], data[SWAP_HOST_] / MEGA, '-o', label=SWAP_HOST_, color='orange')
-ax2.set_xlabel('Time')
 ax2.set_ylabel('Memory (Mo)')
 ax2.set_title('Memory Usage Over Time')
 # tiny legend
@@ -53,7 +54,6 @@ ax2.grid(True)
 
 # Third plot for response time
 ax3.plot(data['Time'], data[RESPONSE_TIME_VM_], '-o', label=RESPONSE_TIME_VM_, color='blue')
-ax3.set_xlabel('Time')
 ax3.set_ylabel('Response Time (ms)')
 ax3.set_title('Response Time Over Time')
 ax3.grid(True)
